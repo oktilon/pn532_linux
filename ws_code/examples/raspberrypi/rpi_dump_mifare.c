307ae709a4a1437d43ff82267b8efef7c9561f3c
Denys <oktilon@gmail.com>
2026-02-11 00:34:44 +0100

    loop scan

--- ws_code/examples/raspberrypi/rpi_dump_mifare.c
+++ ws_code/examples/raspberrypi/rpi_dump_mifare.c
@@ -21,40 +21,44 @@ int main(int argc, char** argv) {
         return -1;
     }
     PN532_SamConfiguration(&pn532);
-    printf("Waiting for RFID/NFC card...\r\n");
-    while (1)
-    {
-        // Check if a card is available to read
-        uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
-        if (uid_len == PN532_STATUS_ERROR) {
-            printf(".");
-        } else {
-            printf("Found card with UID: ");
-            for (uint8_t i = 0; i < uid_len; i++) {
-                printf("%02x ", uid[i]);
+    while (1) {
+        printf("Waiting for RFID/NFC card...\r\n");
+        while (1) {
+            // Check if a card is available to read
+            uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
+            if (uid_len == PN532_STATUS_ERROR) {
+                printf(".");
+            } else {
+                printf("Found card with UID: ");
+                for (uint8_t i = 0; i < uid_len; i++) {
+                    printf("%02x ", uid[i]);
+                }
+                printf("\r\n");
+                break;
+            }
+        }
+        printf("Reading blocks...\r\n");
+        for (uint8_t block_number = 0; block_number < 64; block_number++) {
+            printf("Auth block %hhu\r\n", block_number);
+            pn532_error = PN532_MifareClassicAuthenticateBlock(&pn532, uid, uid_len,
+                    block_number, MIFARE_CMD_AUTH_A, key_a);
+            if (pn532_error != PN532_ERROR_NONE) {
+                printf("Auth block %hhu error 0x%02X\r\n", block_number, pn532_error);
+                continue;
+            }
+            pn532_error = PN532_MifareClassicReadBlock(&pn532, buff, block_number);
+            if (pn532_error != PN532_ERROR_NONE) {
+                printf("Auth block %hhu error 0x%02X\r\n", block_number, pn532_error);
+                continue;
+            }
+            printf("%d: ", block_number);
+            for (uint8_t i = 0; i < 16; i++) {
+                printf("%02x ", buff[i]);
             }
             printf("\r\n");
-            break;
         }
-    }
-    printf("Reading blocks...\r\n");
-    for (uint8_t block_number = 0; block_number < 64; block_number++) {
-        pn532_error = PN532_MifareClassicAuthenticateBlock(&pn532, uid, uid_len,
-                block_number, MIFARE_CMD_AUTH_A, key_a);
-        if (pn532_error != PN532_ERROR_NONE) {
-            break;
+        if (pn532_error) {
+            printf("Error: 0x%02x\r\n", pn532_error);
         }
-        pn532_error = PN532_MifareClassicReadBlock(&pn532, buff, block_number);
-        if (pn532_error != PN532_ERROR_NONE) {
-            break;
-        }
-        printf("%d: ", block_number);
-        for (uint8_t i = 0; i < 16; i++) {
-            printf("%02x ", buff[i]);
-        }
-        printf("\r\n");
-    }
-    if (pn532_error) {
-        printf("Error: 0x%02x\r\n", pn532_error);
     }
 }
